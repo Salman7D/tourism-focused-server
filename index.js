311f626d52e8dd848d28baf2f1ee1c6b727072b6
Salman7D <salmanrayhan753@gmail.com>
2024-04-27 16:26:06 +0600

    delete data from the server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,15 @@ async function run() {
         res.send(result);
     })
 
+
+    app.delete("/touristsSpot/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {
+            _id: new ObjectId(id)
+        }
+        const result = await touristsSpotCollection.deleteOne(query);
+        res.send(result);
+    })
 
 
 
